2d989413a922b6cdda491e6402e530836f57a951
Austin Hughes <austindavidhughes@gmail.com>
2016-03-23 10:56:48 -0600

    Generating random verification codes.

--- app/api/public.js
+++ app/api/public.js
@@ -3,17 +3,30 @@ var express = require( 'express' );
 var router = express.Router();
 var twilio = require( '../texting' );
 
+var CODES = {};
+
+function getRandomInt( min, max ) {
+	return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+}
+
 router.post( '/verificationCode', function( req, res ) {
 
 	var phoneNumber = req.body.number;
 
-	var code = '123456'; //This should be replaced with some kind of random number generator
-						 //and saved in the database to check when they enter the number back in...
-						 //probably add expiration date to this code as well
+	//Get code to text back and save in object
+	var code = getRandomInt( 100000, 999999 ); //Random 6 digit number
+	CODES[phoneNumber] = code;
 
-	console.log( 'phoneNumber: ', phoneNumber );
+	//Expire code after 5 minutes
+	setTimeout( function() {
+		if ( CODES[phoneNumber] === code ) { //to check that it hasn't been replaced with a new code
+			delete CODES[phoneNumber];
+		}
+	}, 5 * 1000 * 60 );
 
-	twilio.sendMessage( code, phoneNumber, function( err, data ) {
+	var message = 'Your verification code is: ' + code;
+
+	twilio.sendMessage( message, phoneNumber, function( err, data ) {
 
 		if ( err ) {
 			console.log( 'err: ', err );
